66e0abd8c1326d5fe321f3620ac906c9adce1b7b
Kristy Gray <thekristyleigh@gmail.com>
2023-12-12 15:01:47 +0800

    Add HelloWorld program code structure alternatives
    Old version (inc namespace), new version (implied namespace) + alternative use of args

--- kristy-dotnet-course-code/Section2_csharp_crash_course_basics/HelloWorld/Program.cs
+++ kristy-dotnet-course-code/Section2_csharp_crash_course_basics/HelloWorld/Program.cs
@@ -1,8 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# programmers");
-Console.Write("First");
-Console.Write("Second");
-Console.WriteLine("Hello, C# programmers");
-Console.Write("First");
-Console.Write("Second"); 
+﻿/* _____________________________________________________
+
+    Hello World
+_______________________________________________________*/
+
+// See https://aka.ms/new-console-template for more information
+
+// ----------------------
+// new version:
+// ----------------------
+
+// Console.WriteLine("Hello, C# programmers");
+// Console.Write("First");
+// Console.Write("Second");
+// Console.WriteLine("Hello, C# programmers");
+// Console.Write("First");
+// Console.Write("Second"); 
 // we are inside a method/class
+
+// ----------------------
+// alternative (passing in arguments):
+// ----------------------
+
+// Console.WriteLine(args[0]);
+// in terminal: dotnet run <argument to print>
+
+// ----------------------
+// old version: 
+// Will be used for course to help understanding
+// ----------------------
+
+using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        // method: something a class can do
+        // Main runs by default
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
