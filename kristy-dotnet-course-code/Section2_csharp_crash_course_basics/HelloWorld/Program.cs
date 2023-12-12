fa341e75c049c40ecc908b6acedcd2e81de17904
Kristy Gray <thekristyleigh@gmail.com>
2023-12-12 16:22:13 +0800

    Update formatting for visibility

--- kristy-dotnet-course-code/Section2_csharp_crash_course_basics/HelloWorld/Program.cs
+++ kristy-dotnet-course-code/Section2_csharp_crash_course_basics/HelloWorld/Program.cs
@@ -64,68 +64,68 @@ namespace HelloWorld
             // BYTE, SBYTE
             // ----------------------
 
-            // // 1 byte (8 bit) unsigned, where unsigned means it can't be negative
-            // byte myByte = 255;  
-            // byte mySecondByte = 0; 
+            // 1 byte (8 bit) unsigned, where unsigned means it can't be negative
+            byte myByte = 255;  
+            byte mySecondByte = 0; 
  
-            // // 1 byte (8 bit) signed, where signed means it can be negative
-            // sbyte mySbyte = 127;  
-            // sbyte mySecondSbyte = -128; 
+            // 1 byte (8 bit) signed, where signed means it can be negative
+            sbyte mySbyte = 127;  
+            sbyte mySecondSbyte = -128; 
 
             // ----------------------
             // USHORT, SHORT
             // ----------------------            
 
-            // // 2 byte (16 bit) unsigned, where signed means it can be negative
-            // ushort myUshort = 65535;
+            // 2 byte (16 bit) unsigned, where signed means it can be negative
+            ushort myUshort = 65535;
  
-            // // 2 byte (16 bit) signed, where signed means it can be negative
-            // short myShort = -32768;
+            // 2 byte (16 bit) signed, where signed means it can be negative
+            short myShort = -32768;
 
             // ----------------------
             // INT, LONG
             // ----------------------            
  
-            // // 4 byte (32 bit) signed, where signed means it can be negative
-            // int myInt = 2147483647;
-            // int mySecondInt = -2147483648;
+            // 4 byte (32 bit) signed, where signed means it can be negative
+            int myInt = 2147483647;
+            int mySecondInt = -2147483648;
  
-            // // 8 byte (64 bit) signed, where signed means it can be negative
-            // long myLong = -9223372036854775808;
+            // 8 byte (64 bit) signed, where signed means it can be negative
+            long myLong = -9223372036854775808;
 
             // ----------------------
             // FLOAT, DOUBLE, DECIMAL
             // ----------------------  
  
-            // // 4 byte (32 bit) floating point number
-            // float myFloat = 0.751f;
-            // float mySecondFloat = 0.75f;
+            // 4 byte (32 bit) floating point number
+            float myFloat = 0.751f;
+            float mySecondFloat = 0.75f;
  
-            // // 8 byte (64 bit) floating point number
-            // double myDouble = 0.751;
-            // double mySecondDouble = 0.75d;
+            // 8 byte (64 bit) floating point number
+            double myDouble = 0.751;
+            double mySecondDouble = 0.75d;
  
-            // // 16 byte (128 bit) floating point number
-            // decimal myDecimal = 0.751m;
-            // decimal mySecondDecimal = 0.75m;
+            // 16 byte (128 bit) floating point number
+            decimal myDecimal = 0.751m;
+            decimal mySecondDecimal = 0.75m;
 
             // ----------------------
             // TEST PRECISION
             // ----------------------              
  
-            // Console.WriteLine(myFloat - mySecondFloat);
-            // Console.WriteLine(myDouble - mySecondDouble);
-            // Console.WriteLine(myDecimal - mySecondDecimal);
+            Console.WriteLine(myFloat - mySecondFloat);
+            Console.WriteLine(myDouble - mySecondDouble);
+            Console.WriteLine(myDecimal - mySecondDecimal);
  
             // ----------------------
             // STRING, CHAR
             // ---------------------- 
             
-            // string myString = "Hello World";
-            // Console.WriteLine(myString);
+            string myString = "Hello World";
+            Console.WriteLine(myString);
 
-            // char myChar = 'H';
-            // Console.WriteLine(myChar);
+            char myChar = 'H';
+            Console.WriteLine(myChar);
 
             // char myChar2 = "H";
             // Console.WriteLine(myChar2); // ERROR: can't use  ""
@@ -133,31 +133,30 @@ namespace HelloWorld
             // char myChar3 = 'He';
             // Console.WriteLine(myChar3); // ERROR: > 1 character
 
-            // string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
-            // Console.WriteLine(myStringWithSymbols);
+            string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
+            Console.WriteLine(myStringWithSymbols);
  
             // ----------------------
             // BOOL, SYSTEM.BOOLEAN
             // ---------------------- 
 
-            // bool myBool = true;
-            // Console.WriteLine(myBool);
+            bool myBool = true;
+            Console.WriteLine(myBool);
 
             // bool myBool2 = True; // ERROR: must be lowercase
             // Console.WriteLine(myBool2);
 
-            // // same as:
-            // System.Boolean myBool3 = true;
-            // // bool = alias of System.Boolean
-            // Console.WriteLine(myBool3);
+            // same as:
+            System.Boolean myBool3 = true; // bool = alias of System.Boolean
+            Console.WriteLine(myBool3);
 
             // ----------------------
             // GETTYPE()
             // ---------------------- 
+
             bool myBoolean = true;
             Console.WriteLine(myBoolean.GetType());
             // check variable type
-
         }
     }
 }
